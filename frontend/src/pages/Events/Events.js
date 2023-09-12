6fcbea1636bf6e0724430d0e8d09d713c7bf2cbb
Nicolas Peredo <nicolas.peredo@globant.com>
2023-09-12 09:06:54 -0300

    Configuring project and initial routes logic

---
+++ frontend/src/pages/Events/Events.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DUMMY_EVENTS = [
+  { id: "event-1", name: "event-1" },
+  { id: "event-2", name: "event-2" },
+  { id: "event-3", name: "event-3" },
+  { id: "event-4", name: "event-4" },
+];
+export default function Events() {
+  return <div>Events</div>;
+}
