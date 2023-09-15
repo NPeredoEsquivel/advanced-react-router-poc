a76579f4d4161dc8eb94e33f3cf516ab2cf19b26
Nicolas Peredo <nicolas.peredo@globant.com>
2023-09-15 09:49:13 -0300

    Adding new files

---
+++ frontend/src/pages/Authentication/Authentication.js
@@ -0,0 +1,43 @@
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../../components/AuthForm/AuthForm";
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export default AuthenticationPage;
+
+export const action = async ({ request }) => {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "signup" && mode !== "login") {
+    throw json({ messag: "Invalid action" }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  console.log(data);
+  const userData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  let url = "http://localhost:8080/" + mode;
+
+  const response = await fetch(url, {
+    method: request.method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: "Could not login." }, { status: 500 });
+  }
+
+  return redirect("/");
+};
